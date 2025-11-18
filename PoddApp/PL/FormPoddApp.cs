7882badcd262b604352827d1e80943db6a5d01dc
LinneaLex <linnea.lekter@gmail.com>
2025-11-18 13:59:30 +0100

    Lagt in design för formPoddApp, huvudformuläret.

--- PoddApp/PL/FormPoddApp.cs
+++ PoddApp/PL/FormPoddApp.cs
@@ -1,8 +1,8 @@
 namespace PL
 {
-    public partial class Form1 : Form
+    public partial class FormPoddApp : Form
     {
-        public Form1()
+        public FormPoddApp()
         {
             InitializeComponent();
         }

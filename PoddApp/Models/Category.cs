5b242ccd5337d51264941fe482d2b6ce4306e703
Majar4 <majaraiha@outlook.com>
2025-11-20 12:15:49 +0100

    lagt till bson i category

--- PoddApp/Models/Category.cs
+++ PoddApp/Models/Category.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +12,18 @@ namespace Models
     public class Category
     {
         public string Name { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string CategoryID { get; set; }
 
         public Category(string name)
         {
             Name = name;
+        }
+
+        public Category()
+        {
+
         }
     }
 }

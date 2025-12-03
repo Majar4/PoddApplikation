ed9d07a8caf68e75b72a665fc273dc9002119439
Majar4 <majaraiha@outlook.com>
2025-12-03 07:45:01 +0100

    mongodbservice
    Fixat MongoDBService, kopplat bort comboboxarna från varandra

---
+++ PoddApp/BL/MongoDBService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Models;
+
+namespace BL
+{
+    public class MongoDBService : IMongoDBService
+    {
+       
+        private readonly string _connectionString;
+        private readonly string _databaseName;
+        private readonly HttpClient _http;
+
+        public MongoDBService(string connectionString, string databaseName)
+        {
+            _connectionString = connectionString;
+            _databaseName = databaseName;
+            _http = new HttpClient();
+        }
+        
+        public IPodcastService createPodcastService()
+        {
+            var rssClient = new RssReaderClient(_http);
+            var podcastRepo = new PodcastRepository(_connectionString, _databaseName);
+            return new PodcastService(podcastRepo, rssClient);
+
+
+        }
+
+        public ICategoryService createCategoryService()
+        {
+            var categoryRepo = new CategoryRepository(_connectionString, _databaseName);
+            return new CategoryService(categoryRepo);
+        }
+    }
+}
